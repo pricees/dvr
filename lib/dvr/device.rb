a2b6d9f297e02432b226f3b3b136aa8a9887fa36
Edward Price <ted.price@gmail.com>
2014-05-13 00:40:09 -0500

    a little more on devices

--- lib/dvr/device.rb
+++ lib/dvr/device.rb
@@ -4,15 +4,26 @@ module Dvr
     def initialize(streams = 1, total_space = 200E9)
       @streams = streams
       @total_space = total_space
-
-    end
-    # Number of streams to download content
-    def streams
-      
+      @recordings = []
     end
 
-    def approx
+    attr_reader :streams
+    attr_reader :total_space
+    attr_accessor :recordings
+
+    #
+    # NOTE: This makes approximations based on the FULL recording size of shows. Partial recordings will be approximated with there full size
+    def space_remaining
+      recordings_size = 0
+      if recordings.any?
+        recordings_size =  recordings.map(&:approximate_space).reduce(:+) 
+      end
+
+      total_space - recordings_size
     end
 
+    def space_remaining?
+      space_remaining > 0
+    end
   end
 end
--- spec/dvr/device_spec.rb
+++ spec/dvr/device_spec.rb
@@ -3,4 +3,31 @@ require_relative "../../lib/dvr.rb"
 
 describe Dvr::Device do
 
+  describe "#space_remaining" do
+    context "with zero recordings" do
+      it "has all space remaining" do
+        expect(subject.space_remaining).to eq(subject.total_space)
+      end
+
+      it "has space available" do
+        expect(subject).to be_space_remaining
+      end
+    end
+
+    context "with recordings" do
+      let (:recordings) do
+        [ double(approximate_space: 1E9),
+          double(approximate_space: 8E9),
+          double(approximate_space: 2E9),]
+      end
+
+      before do
+        subject.recordings = recordings
+      end
+
+      it "has some space remaining" do
+        expect(subject.space_remaining).to eq(189E9)
+      end
+    end
+  end
 end
